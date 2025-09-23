7652ae6b2f510febf1bda20cd0de0f176389b7cc
anisharora1 <anishkumar890@gmail.com>
2025-09-23 23:37:18 +0530

    Add CSP in backemnd and frontend

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,7 @@ app.use(helmet.contentSecurityPolicy({
       "'self'",
       "'unsafe-inline'",
       'https://cdn.jsdelivr.net',
+      'https://clerk.apnahero.in',
       'https://*.clerk.com',
       'https://*.clerk.accounts.dev',
       'https://*.clerk.services',
